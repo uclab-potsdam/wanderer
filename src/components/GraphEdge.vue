05521bcd1d8dae197678d2581c1a214be5de4f43
fidelthomet <git@ft0.ch>
2024-02-26 14:02:52 +0100

    added markers

--- src/components/GraphEdge.vue
+++ src/components/GraphEdge.vue
@@ -15,15 +15,46 @@ const d = computed(() => {
   if (source.value == null || target.value == null) return
   return `M${source.value?.x},${source.value?.y} L${target.value?.x},${target.value?.y}`
 })
+
+const id = computed(() => props.edge.nodes.join('-'))
+const markerId = computed(() => `url('#marker-${id.value}')`)
+const markerEnd = computed(
+  () => (props.edge.arrow === '→' || props.edge.arrow === '↔') && markerId.value
+)
+const markerStart = computed(
+  () => (props.edge.arrow === '←' || props.edge.arrow === '↔') && markerId.value
+)
 </script>
 
 <template>
-  <path class="edge" :d="d" />
+  <g class="edge">
+    <defs>
+      <marker
+        :id="`marker-${id}`"
+        markerWidth="10"
+        markerHeight="20"
+        refX="10"
+        refY="10"
+        orient="auto"
+      >
+        <path d="M0,0 L10,10 L0,20" />
+      </marker>
+    </defs>
+    <path :d="d" :marker-end="markerEnd" :marker-start="markerStart" />
+  </g>
 </template>
 
 <style scoped>
 .edge {
   stroke: black;
   transition: d var(--transition);
+
+  marker {
+    overflow: visible;
+    path {
+      stroke: var(--marker);
+      fill: none;
+    }
+  }
 }
 </style>
