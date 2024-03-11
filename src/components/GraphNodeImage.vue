d0b343e8b202e62742d1a1b243128e5b3cc17a97
fidelthomet <git@ft0.ch>
2024-03-11 14:46:28 +0100

    image tinting

--- src/components/GraphNodeImage.vue
+++ src/components/GraphNodeImage.vue
@@ -18,11 +18,30 @@ defineExpose({
 </script>
 
 <template>
-  <img ref="el" class="image" :srcset="`${url} 2x`" />
+  <div class="image">
+    <img ref="el" :srcset="`${url} 2x`" />
+  </div>
 </template>
 
 <style scoped>
 .image {
+  pointer-events: none;
   z-index: -2;
+
+  background: var(--graph-accent);
+
+  img {
+    background: var(--color-background);
+    mix-blend-mode: hard-light;
+    display: block;
+    transform: scale(1.005);
+  }
+
+  .highlight {
+    background: transparent;
+    img {
+      mix-blend-mode: none;
+    }
+  }
 }
 </style>
