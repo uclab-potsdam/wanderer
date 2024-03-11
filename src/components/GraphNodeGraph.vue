9f36dc6de832c484882d402996400d5214044b68
fidelthomet <git@ft0.ch>
2024-03-11 12:09:40 +0100

    tinted graph nodes

--- src/components/GraphNodeGraph.vue
+++ src/components/GraphNodeGraph.vue
@@ -14,6 +14,9 @@ const el = ref(null)
 
 const text = computed(() => helperStore.localize(props.node.text))
 const image = computed(() => `url("${helperStore.getMediaUrl(props.node.image)}")`)
+const color = computed(() => {
+  return { '--graph-accent': `var(--${props.node.color})` }
+})
 
 defineExpose({
   el
@@ -21,8 +24,8 @@ defineExpose({
 </script>
 
 <template>
-  <div class="entity" ref="el" :style="{ 'background-image': image }">
-    <span class="text" ref="textElement">{{ text }}</span>
+  <div class="entity" ref="el" :style="{ 'background-image': image, ...color }">
+    <div class="text" ref="textElement">{{ text }}</div>
   </div>
 </template>
 
@@ -30,15 +33,22 @@ defineExpose({
 .entity {
   width: 250px;
   height: 150px;
-  border: 1px solid;
-  padding: calc(var(--spacing) * 0.5);
+  border: 1px solid var(--graph-accent);
   border-radius: calc(var(--spacing) * 0.25);
   z-index: -1;
 
   background-size: cover;
   background-position: center center;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+
   .text {
     font-weight: 900;
+    padding: calc(var(--spacing) * 0.5);
+    color: white;
+    background-color: color-mix(in lab, var(--graph-accent), transparent 30%);
   }
 }
 </style>
