1c447b6d3c88106714bf83a051c1a6794c36b16b
fidelthomet <git@ft0.ch>
2024-03-11 15:45:00 +0100

    styled node graph nodes

--- src/components/GraphNodeGraph.vue
+++ src/components/GraphNodeGraph.vue
@@ -1,22 +1,29 @@
 <script setup>
 import { computed, ref } from 'vue'
 import { useHelperStore } from '@/stores/helper'
+import { useVideoStore } from '@/stores/video'
+
+import IconPlay from '~icons/base/IconPlay'
+import IconPlaying from '@/components/IconPlaying.vue'
 
 const props = defineProps({
-  node: Object
+  node: Object,
+  id: String
 })
 
 const helperStore = useHelperStore()
+const videoStore = useVideoStore()
 
 const textElement = ref(null)
 
 const el = ref(null)
 
 const text = computed(() => helperStore.localize(props.node.text))
-const image = computed(() => `url("${helperStore.getMediaUrl(props.node.image)}")`)
+const image = computed(() => helperStore.getMediaUrl(props.node.image))
 const color = computed(() => {
   return { '--graph-accent': `var(--${props.node.color})` }
 })
+const playing = computed(() => videoStore.graphId === props.id && videoStore.playing)
 
 defineExpose({
   el
@@ -24,8 +31,13 @@ defineExpose({
 </script>
 
 <template>
-  <div class="entity" ref="el" :style="{ 'background-image': image, ...color }">
-    <div class="text" ref="textElement">{{ text }}</div>
+  <div class="entity" ref="el" :style="color">
+    <img :src="image" />
+    <div class="text" ref="textElement">
+      {{ text }}
+      <IconPlay v-if="!playing" />
+      <IconPlaying v-else />
+    </div>
   </div>
 </template>
 
@@ -33,9 +45,12 @@ defineExpose({
 .entity {
   width: 250px;
   height: 150px;
-  border: 1px solid var(--graph-accent);
+  /* border: 1px solid var(--graph-accent); */
   border-radius: calc(var(--spacing) * 0.25);
+  overflow: hidden;
   z-index: -1;
+
+  background-color: color-mix(in lab, var(--graph-accent), var(--color-text) 60%);
 
   background-size: cover;
   background-position: center center;
@@ -44,11 +59,32 @@ defineExpose({
   flex-direction: column;
   justify-content: flex-end;
 
+  img {
+    width: 100%;
+    height: 100%;
+    position: absolute;
+    object-fit: cover;
+    z-index: -1;
+    /* filter: grayscale(1); */
+    /* mix-blend-mode: hard-light; */
+    /* mix-blend-mode: luminosity; */
+  }
+
   .text {
     font-weight: 900;
     padding: calc(var(--spacing) * 0.5);
-    color: white;
-    background-color: color-mix(in lab, var(--graph-accent), transparent 30%);
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+
+    color: color-mix(in lab, var(--graph-accent), var(--color-background) 100%);
+    background-color: color-mix(
+      in lab,
+      color-mix(in lab, var(--graph-accent), var(--color-text) 60%),
+      transparent 40%
+    );
+    backdrop-filter: blur(7px);
   }
 }
 </style>
--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,25 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import Icons from 'unplugin-icons/vite'
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Icons({
+      compiler: 'vue3',
+      customCollections: {
+        // key as the collection name
+        base: FileSystemIconLoader('./src/assets/icons', (svg) =>
+          svg
+            .replace(/fill="#000000"/gi, 'fill="currentColor"')
+            .replace(/<svg/, '<svg class="icon"')
+        )
+      }
+    })
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
