eef41102328cf4cbc028d5e478b3cbb138b92b52
fidelthomet <git@ft0.ch>
2024-03-26 10:04:45 -0300

    hash history

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import { useDataStore } from '@/stores/data'
 import { useVideoStore } from '@/stores/video'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
