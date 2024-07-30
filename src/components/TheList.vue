8c5e9a440977dbdb97e345d0d45ab155f9aeb34c
fidelthomet <git@ft0.ch>
2024-07-30 15:14:37 +0200

    close #129

--- src/components/TheList.vue
+++ src/components/TheList.vue
@@ -4,20 +4,25 @@ import { useRoute } from 'vue-router'
 import ListItem from './ListItem.vue'
 import ListWrapper from './ListWrapper.vue'
 import InputButton from './InputButton.vue'
+import { useModalStore } from '@/stores/modal'
 
 const dataStore = useDataStore()
+const modalStore = useModalStore()
 
 const route = useRoute()
 </script>
 
 <template>
   <main class="list">
-    <ListWrapper class="nav" horizontal equal-size>
-      <InputButton tag="RouterLink" to="projects"> Projects </InputButton>
-      <InputButton tag="RouterLink" to="graph"> Stories </InputButton>
-      <InputButton tag="RouterLink" to="entity"> Entities </InputButton>
-      <InputButton tag="RouterLink" to="image"> Images </InputButton>
-    </ListWrapper>
+    <nav>
+      <ListWrapper horizontal equal-size>
+        <InputButton tag="RouterLink" to="projects"> Projects </InputButton>
+        <InputButton tag="RouterLink" to="graph"> Stories </InputButton>
+        <InputButton tag="RouterLink" to="entity"> Entities </InputButton>
+        <InputButton tag="RouterLink" to="image"> Images </InputButton>
+      </ListWrapper>
+    </nav>
+
     <template v-for="(node, id) in dataStore.data.nodes" :key="id">
       <ListItem
         tag="RouterLink"
@@ -26,7 +31,7 @@ const route = useRoute()
         :label="node.label"
         :meta="dataStore.data.nodes[node.class]?.label"
       >
-        edit
+        <InputButton @click.stop.prevent="modalStore.open(id, 'node')">edit</InputButton>
       </ListItem>
     </template>
   </main>
@@ -37,8 +42,11 @@ const route = useRoute()
   grid-column: main-start / main-end;
   grid-row: main-start / main-end;
 
-  .nav {
+  nav {
     margin: var(--spacing) 0;
+    display: flex;
+    gap: var(--spacing);
+    justify-content: space-between;
   }
 }
 </style>
